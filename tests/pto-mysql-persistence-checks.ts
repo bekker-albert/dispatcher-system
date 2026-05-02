1c65ecad63106192c7aac81d8b8e70d47e03cee8
bekker-albert <255094157+bekker-albert@users.noreply.github.com>
2026-05-02 23:49:00 +0500

    Split PTO MySQL persistence checks

---
+++ tests/pto-mysql-persistence-checks.ts
@@ -0,0 +1,52 @@
+import assert from "node:assert/strict";
+import {
+  loadPtoStateFromMysqlForYear,
+  deletePtoYearFromMysql,
+  savePtoDayValueWithRowToMysql,
+  savePtoDayValuesWithRowToMysql,
+  savePtoStateToMysql,
+} from "../lib/server/mysql/pto";
+import { dbTransaction } from "../lib/server/mysql/pool";
+
+const mysqlTransactionSource = dbTransaction.toString();
+assert.match(mysqlTransactionSource, /ensureMysqlSchema[\s\S]*beginTransaction/);
+assert.match(mysqlTransactionSource, /commit\(\)/);
+assert.match(mysqlTransactionSource, /rollback\(\)/);
+assert.match(mysqlTransactionSource, /release\(\)/);
+
+const mysqlFullSaveSource = savePtoStateToMysql.toString();
+assert.match(mysqlFullSaveSource, /writePtoTransaction/);
+assert.match(mysqlFullSaveSource, /assertMysqlPtoMatchesExpectedUpdatedAt\(state,\s*options\.expectedUpdatedAt,\s*\{[\s\S]*yearScope: options\.yearScope[\s\S]*\}\)/);
+assert.match(mysqlFullSaveSource, /upsertPtoRows\(rowRecords,\s*execute\)/);
+assert.match(mysqlFullSaveSource, /deletePtoDayValuesMissingFromState\("plan",\s*planRows,\s*execute,\s*\{ yearScope: options\.yearScope \}\)/);
+assert.match(mysqlFullSaveSource, /const isYearScopedSave = Boolean\(options\.yearScope\)/);
+assert.match(mysqlFullSaveSource, /if \(options\.yearScope\) \{[\s\S]*prunePtoYearFromRows\(options\.yearScope,\s*execute,\s*\{[\s\S]*excludeRowIdsByTable:[\s\S]*ptoPlanRowIds\)\(planRows\)[\s\S]*ptoPlanRowIds\)\(operRows\)[\s\S]*ptoPlanRowIds\)\(surveyRows\)[\s\S]*\}\);[\s\S]*deletePtoRowsWithoutData\(execute\);[\s\S]*\}/);
+assert.match(mysqlFullSaveSource, /if \(!isYearScopedSave\) \{[\s\S]*deletePtoRowsMissingFromState\("plan",\s*planRows,\s*execute\)[\s\S]*\}/);
+assert.match(mysqlFullSaveSource, /if \(!isYearScopedSave\) \{[\s\S]*deletePtoBucketValuesMissingFromState\(bucketValueRecords,\s*execute\)[\s\S]*deletePtoBucketRowsMissingFromState\(bucketRowRecords,\s*execute\)[\s\S]*\}/);
+assert.match(mysqlFullSaveSource, /deletePtoBucketRowsMissingFromState\(bucketRowRecords,\s*execute\)/);
+
+const mysqlDeleteYearSource = deletePtoYearFromMysql.toString();
+assert.match(mysqlDeleteYearSource, /DELETE FROM pto_day_values WHERE work_date >= \? AND work_date <= \?/);
+assert.match(mysqlDeleteYearSource, /prunePtoYearFromRows\(year,\s*execute\)/);
+assert.match(mysqlDeleteYearSource, /deletePtoRowsWithoutData\(execute\)/);
+
+const mysqlDayWithRowSaveSource = savePtoDayValueWithRowToMysql.toString();
+assert.match(mysqlDayWithRowSaveSource, /writePtoTransaction/);
+assert.ok(mysqlDayWithRowSaveSource.indexOf("insertPtoRowsIfMissing") < mysqlDayWithRowSaveSource.indexOf("upsertPtoDayValues"));
+assert.doesNotMatch(mysqlDayWithRowSaveSource, /upsertPtoRows/);
+
+const mysqlDaysWithRowSaveSource = savePtoDayValuesWithRowToMysql.toString();
+assert.match(mysqlDaysWithRowSaveSource, /writePtoTransaction/);
+assert.ok(mysqlDaysWithRowSaveSource.indexOf("insertPtoRowsIfMissing") < mysqlDaysWithRowSaveSource.indexOf("upsertPtoDayValues"));
+assert.doesNotMatch(mysqlDaysWithRowSaveSource, /upsertPtoRows/);
+
+const mysqlYearLoadSource = loadPtoStateFromMysqlForYear.toString();
+assert.match(mysqlYearLoadSource, /ptoYearDateRange/);
+assert.match(mysqlYearLoadSource, /FROM pto_rows AS rows_for_year/);
+assert.match(mysqlYearLoadSource, /JSON_CONTAINS\(COALESCE\(rows_for_year\.years, JSON_ARRAY\(\)\), JSON_QUOTE\(\?\)\)/);
+assert.match(mysqlYearLoadSource, /JSON_EXTRACT\(COALESCE\(rows_for_year\.carryovers, JSON_OBJECT\(\)\), \?\) IS NOT NULL/);
+assert.match(mysqlYearLoadSource, /EXISTS \([\s\S]*FROM pto_day_values AS values_for_year/);
+assert.match(mysqlYearLoadSource, /SELECT \* FROM pto_day_values[\s\S]*WHERE work_date >= \? AND work_date <= \?/);
+assert.match(mysqlYearLoadSource, /\[start,\s*end\]/);
+assert.doesNotMatch(mysqlYearLoadSource, /ptoDayValueRecordsForYear/);
+assert.match(mysqlYearLoadSource, /SELECT \* FROM pto_bucket_rows ORDER BY sort_index ASC/);
