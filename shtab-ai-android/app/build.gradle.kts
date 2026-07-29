2c5cb27b092cd3541099342e1be073d8f442fa3e
bekker-albert <bekker.albert@internet.ru>
2026-07-29 08:26:37 +0500

    Обновить версию приложения до 4.1

--- shtab-ai-android/app/build.gradle.kts
+++ shtab-ai-android/app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "kz.shtabai.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 16
-        versionName = "4.0.0"
+        versionCode = 17
+        versionName = "4.1.0"
     }
     signingConfigs {
         create("release") {
