482f407e4062c27b3a8c369fc7db2ca8e46e0e10
bekker-albert <bekker.albert@internet.ru>
2026-08-02 18:51:32 +0500

    Expose read-through state handle for 4.7.0 regression audits

---
+++ shtab-ai-android/app/src/main/assets/chunk45.js
@@ -0,0 +1,5 @@
+(function(){
+  Object.defineProperty(window,'__shtabStateV470',{configurable:true,get:()=>state});
+  window.__shtabDiagnostics={...(window.__shtabDiagnostics||{}),uiVersion:()=> '4.7.0'};
+})();
+//# sourceURL=chunk45.js
