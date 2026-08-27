b62b8df3d43e411ba82a0499006798c29ab1d088
bekker-albert <bekker.albert@internet.ru>
2026-08-27 14:03:17 +0500

    Route dispatch dictionaries through shared database API

--- lib/server/database/handlers.ts
+++ lib/server/database/handlers.ts
@@ -1,4 +1,5 @@
 import { handleAppStateDatabaseAction } from "./app-state";
+import { handleDispatchDatabaseAction } from "./dispatch";
 import { handlePtoDatabaseAction } from "./pto";
 import { handleSettingsDatabaseAction } from "./settings";
 import type { DatabaseResourceHandler } from "./types";
@@ -6,6 +7,7 @@ import { handleVehiclesDatabaseAction } from "./vehicles";
 
 export const databaseResourceHandlers: Record<string, DatabaseResourceHandler> = {
   "app-state": handleAppStateDatabaseAction,
+  dispatch: handleDispatchDatabaseAction,
   pto: handlePtoDatabaseAction,
   settings: handleSettingsDatabaseAction,
   vehicles: handleVehiclesDatabaseAction,
